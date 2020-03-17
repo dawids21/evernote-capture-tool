298e826ffd0154b5b42b5e91f2a6257f2eb5e2bc
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-03-17 08:48:10 +0100

    Read token from file

--- src/main/java/evernote_capture_tool/App.java
+++ src/main/java/evernote_capture_tool/App.java
@@ -1,11 +1,16 @@
 package evernote_capture_tool;
 
+import java.io.File;
+import java.util.Scanner;
+
 public class App {
-    private static final String AUTH_TOKEN = "S=s1:U=95c04:E=1783efc2d38:C=170e74affe8:P=1cd:A=en-devtoken:V=2:H=ba83a3eecd53f8d98de6b9b5653ce614";
     
     public static void main(String[] args) throws Exception {
+        File file = new File("token.txt");
+        Scanner input = new Scanner(file);
+        String token = input.nextLine();
+        input.close();
         String capture = args[0];
-        String token = AUTH_TOKEN;
         EvernoteAccount evernote = new EvernoteAccount(token);
         evernote.createNote(capture);
     }
